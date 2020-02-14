212ebbdf25912e2f5448dff424372d8a02aa5489
Derrick Park <derrick8689@gmail.com>
2020-02-14 14:57:13 -0800

    Add quicksort and evaluate solution

--- Recursion/Recursion.swift
+++ Recursion/Recursion.swift
@@ -57,3 +57,62 @@ func reverseLines(_ line: Int) {
     // MARK: - YOUR RECURSIVE CODE
     
 }
+
+/// 5. evaluate
+/// Write a recursive function evaluate that accepts a string representing a math expression and computes its value.
+/// - The expression will be "fully parenthesized" and will  consist of + and * on single-digit integers only.
+/// - You can use a helper function. (Do not change the original function header)
+/// - Use Recursion
+/// evaluate("7")                 -> 7
+/// evaluate("(2+2)")             -> 4
+/// evaluate("(1+(2*4))")         -> 9
+/// evaluate("((1+3)+((1+2)*5))") -> 19
+func evaluateHelperExt(_ expr: String, _ i: inout Int) -> Int {
+    if Character(expr[i]).isNumber {
+        return Int(expr[i])!
+    } else {
+        i += 1 // skip (
+        let left = evaluateHelperExt(expr, &i)
+        i += 1
+        let op = expr[i] // +, *
+        i += 1
+        let right = evaluateHelperExt(expr, &i)
+        i += 1 // skip )
+        if op == "+" {
+            return left + right
+        } else {
+            return left * right
+        }
+    }
+}
+
+func evaluateExt(_ expr: String) -> Int {
+    var i = 0
+    return evaluateHelperExt(expr, &i)
+}
+
+/// Without String extension
+func evaluateHelper(_ expr: String, _ i: inout String.Index) -> Int {
+    if expr[i].isNumber {
+        return Int(String(expr[i]))!
+    } else {
+        i = expr.index(after: i)
+        let leftExpr = evaluateHelper(expr, &i)
+        i = expr.index(after: i)
+        let op = String(expr[i])
+        i = expr.index(after: i)
+        let rightExpr = evaluateHelper(expr, &i)
+        i = expr.index(after: i)
+        if op == "+" {
+            return leftExpr + rightExpr
+        } else {
+            return leftExpr * rightExpr
+        }
+    }
+}
+
+func evaluate(_ expr: String) -> Int {
+    var i = expr.startIndex
+    return evaluateHelper(expr, &i)
+}
+
--- SearchingSorting/Sorting.swift
+++ SearchingSorting/Sorting.swift
@@ -100,3 +100,42 @@ func merge<T: Comparable>(_ left: [T], _ right: [T], _ comparator: (T, T) -> Boo
     
     return merged
 }
+
+/// Quick Sort O(n lg n)
+/// Space Complexity O(n) - which should be O(1)
+func quickSortSlow<T: Comparable>(_ arr: [T]) -> [T] {
+    if arr.count <= 1 { return arr }
+    var arr = arr
+    let pivot = arr.removeLast()
+    let lessOrEqual = arr.filter { $0 <= pivot }
+    let greater = arr.filter { $0 > pivot }
+    return quickSortSlow(lessOrEqual) + [pivot] +  quickSortSlow(greater)
+}
+
+/// Quick Sort O(n lg n)
+/// Space Complexity O(1) (in-place)
+func quickSort<T: Comparable>(_ arr: inout [T], _ start: Int, _ end: Int) {
+    if start < end {
+        let pivotIndex = partition(&arr, start, end)
+        quickSort(&arr, start, pivotIndex - 1)
+        quickSort(&arr, pivotIndex + 1, end)
+    }
+}
+
+/// Returns the index of the pivot.
+/// - Parameters:
+///   - arr: the array
+///   - start: start index
+///   - end: end index
+func partition<T: Comparable>(_ arr: inout [T], _ start: Int, _ end: Int) -> Int {
+    var pivotIndex = start
+    let pivot = arr[end]
+    for i in start..<end {
+        if arr[i] < pivot {
+            arr.swapAt(pivotIndex, i)
+            pivotIndex += 1
+        }
+    }
+    arr.swapAt(pivotIndex, end)
+    return pivotIndex
+}
